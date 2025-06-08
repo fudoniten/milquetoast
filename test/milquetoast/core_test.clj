6ec857b97d2b294a17edc5a4977a3670ef66811d
niten <niten@fudo.org>
2025-06-08 10:06:59 -0700

    test: Update client tests to use direct references for client types

--- test/milquetoast/core_test.clj
+++ test/milquetoast/core_test.clj
@@ -1,49 +1,13 @@
 (ns milquetoast.core-test
-  (:require [clojure.test :refer :all]
-            [milquetoast.core :as core]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [milquetoast.core :as core :refer [MilquetoastClient MilquetoastJsonClient]]))
 
 (deftest test-create-client
   (testing "create-client"
     (let [client (core/create-client "tcp://localhost:1883" "username" "password")]
-      (is (instance? core.MilquetoastClient client)))))
+      (is (instance? MilquetoastClient client)))))
 
 (deftest test-create-json-client
   (testing "create-json-client"
     (let [client (core/create-json-client "tcp://localhost:1883" "username" "password")]
-      (is (instance? core.MilquetoastJsonClient client)))))
-
-(deftest test-send-message!
-  (testing "send-message!"
-    ;; TODO: Add your test implementation here
-    ;; This test is left empty because it requires a running MQTT broker to test against.
-    ))
-
-(deftest test-stop!
-  (testing "stop!"
-    ;; TODO: Add your test implementation here
-    ;; This test is left empty because it requires a running MQTT broker to test against.
-    ))
-
-(deftest test-add-channel!
-  (testing "add-channel!"
-    ;; TODO: Add your test implementation here
-    ;; This test is left empty because it requires a running MQTT broker to test against.
-    ))
-
-(deftest test-subscribe-topic!
-  (testing "subscribe-topic!"
-    ;; TODO: Add your test implementation here
-    ;; This test is left empty because it requires a running MQTT broker to test against.
-    ))
-
-(deftest test-get-topic!
-  (testing "get-topic!"
-    ;; TODO: Add your test implementation here
-    ;; This test is left empty because it requires a running MQTT broker to test against.
-    ))
-
-(deftest test-get-topic-raw!
-  (testing "get-topic-raw!"
-    ;; TODO: Add your test implementation here
-    ;; This test is left empty because it requires a running MQTT broker to test against.
-    ))
+      (is (instance? MilquetoastJsonClient client)))))
